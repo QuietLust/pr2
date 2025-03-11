1fd037586cd5a4b9c9270cf0cc0eab0a59aba65e
QuietLust <145840708+QuietLust@users.noreply.github.com>
2025-03-11 09:11:19 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -8,12 +8,12 @@
 #include "file_utils.h"
 
 int main(int argc, char *argv[]) {
-    char *log_path = NULL;
-    char *error_path = NULL;
+    char *log_file = NULL;
+    char *error_file = NULL;
     int opt;
-    int sh_u=0, sh_p=0, sh_h=0;
+    int show_users = 0, show_processes = 0, show_help = 0;
 
-    static struct option long_options[] = {
+    static struct option long_opts[] = {
         {"users", no_argument, 0, 'u'},
         {"processes", no_argument, 0, 'p'},
         {"help", no_argument, 0, 'h'},
@@ -22,52 +22,52 @@ int main(int argc, char *argv[]) {
         {0, 0, 0, 0}
     };
 
-    int long_index = 0;
-    while ((opt = getopt_long(argc, argv, "uphl:e:", long_options, &long_index)) != -1) {
-        switch (opt) {
+    int opt_index = 0;
+    while ((option = getopt_long(argc, argv, "uphl:e:", long_opts, &opt_index)) != -1) {
+        switch (option) {
             case 'u':
-                sh_u=1;
+                show_users = 1;
                 break;
             case 'p':
-                sh_p=1;
+                show_processes = 1;
                 break;
             case 'h':
-                sh_h=1;
+                show_help = 1;
                 break;
             case 'l':
-                log_path = optarg;
-                if (check_path(log_path) != 0) {
+                log_file = optarg;
+                if (validate_path(log_file) != 0) {
                     return 1;
                 }
                 break;
             case 'e':
-                error_path = optarg;
-                if (check_path(error_path) != 0) {
+                error_file = optarg;
+                if (validate_path(error_file) != 0) {
                     return 1;
                 }
                 break;
             default:
-                if (error_path != NULL) {
-                    freopen(error_path, "a", stderr);
+                if (error_file != NULL) {
+                    freopen(error_file, "a", stderr);
                 }
                 fprintf(stderr, "Неизвестная опция -%c\n", optopt);
-                print_help();
+                display_help();
                 return 1;
         }
     }
 
-    if (log_path != NULL) {
-        freopen(log_path, "w", stdout);
+    if (log_file != NULL) {
+        freopen(log_file, "w", stdout);
     }
 
-    if (error_path != NULL) {
-        freopen(error_path, "a", stderr);
+    if (error_file != NULL) {
+        freopen(error_file, "a", stderr);
     }
 
-    if (sh_h) {print_help(); return 0;}
-    if (!sh_u && !sh_p) {printf("Не заданы явные аргументы \n"); print_help(); return 1;}
-    if (sh_u) {print_users();}
-    if (sh_p) {print_processes();}
+    if (show_help) {display_help(); return 0;}
+    if (!show_users && !show_processes) {printf("Не заданы явные аргументы \n"); display_help(); return 1;}
+    if (show_users) {display_users();}
+    if (show_processes) {display_processes();}
 
     return 0;
 }
